cb9fafec4e4eeee63b5a1a920a8ec6bcc05aa960
Karoline Correa <ckaroline226@gmail.com>
2022-05-27 19:16:13 -0300

    Adicionando primeiros arquivos

---
+++ script.js
@@ -0,0 +1,19 @@
+const elementoLista = document.querySelector('ul') 
+const elementoInput = document.querySelector('input')
+const elementoBotao = document.querySelector('button')
+
+const tarefas = [
+    
+]
+
+function mostraTarefas () {
+    for (tarefa of tarefas) {
+        const elementoTarefa = document.createElement('li')
+        const textoTarefa = document.createTextNode(tarefa)
+
+        elementoTarefa.appendChild(textoTarefa)
+        elementoLista.appendChild(elementoTarefa)
+    }
+}
+
+mostraTarefas ()
