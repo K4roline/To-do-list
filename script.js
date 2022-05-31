00b58dcaeebcad23d0ede43f5fd95b25f0233ec9
Karoline Correa <ckaroline226@gmail.com>
2022-05-30 22:00:14 -0300

    Atualizando as funcionalidades do JS, manipulando CSS e HTML

--- script.js
+++ script.js
@@ -1,32 +1,64 @@
-const elementoLista = document.querySelector('ul') 
-const elementoInput = document.querySelector('input')
-const elementoBotao = document.querySelector('button')
+// const elementoLista = document.querySelector('ul') 
+// const elementoInput = document.querySelector('input')
+// const elementoBotao = document.querySelector('button')
 
-const tarefas = [
-    //  aqui ficara as tarefas
-]
+// const tarefas = [
+// ]
 
-function mostraTarefas () {
+// function mostraTarefas () {
 
-    elementoLista.innerHTML = ''
+//     elementoLista.innerHTML = ''
 
-    for (tarefa of tarefas) {
-        const elementoTarefa = document.createElement('li')
-        const textoTarefa = document.createTextNode(tarefa)
+//     for (tarefa of tarefas) {
+//         const elementoTarefa = document.createElement('li')
+//         const textoTarefa = document.createTextNode(tarefa)
 
-        elementoTarefa.appendChild(textoTarefa)
-        elementoLista.appendChild(elementoTarefa)
-    }
+//         const elementoLink = document.childElement('a')
+//         const pos = tarefas.indexOf(tarefa)
+
+//         const linkText = document.createTextNode('x')
+//         elementoLink.appendChild(linkText)
+
+//         elementoLink.setAttribute('href', '#')
+//         elementoLink.setAttribute('onclick', `deletaTarefa(${pos})`)
+
+//         elementoTarefa.appendChild(textoTarefa)
+//         elementoLista.appendChild(elementoTarefa)
+//         elementoTarefa.appendChild(elementoLink)
+//     }
+// }
+
+// mostraTarefas ()
+
+// function addTarefa () {
+//     const textoTarefa = elementoInput.value
+//     tarefas.push(textoTarefa)
+//     elementoInput.value = ''
+
+//     mostraTarefas()
+// }
+
+// elementoBotao.setAttribute( 'onclick', 'addTarefa()')
+
+// function deletaTarefa(pos) {
+//     tarefas.splice(pos, 1)
+//     mostraTarefas()
+// }
+
+
+var enterButton = document.getElementById("enter")
+var input = getElementById("userInput")
+var ul = document.getElementById("ul")
+var item = document.getElementsByTagName("li")
+
+function inputLength(){
+    return input.value.length
 }
 
-mostraTarefas ()
+function createListElement(){
+    var li = document.createElement("li")
 
-function addTarefa () {
-    const textoTarefa = elementoInput.value
-    tarefas.push(textoTarefa)
-    elementoInput.value = ''
-
-    mostraTarefas()
+li.appendChild(document.createTextNode(input.value))
+ul.appendChild(li)
+input.value = ""
 }
-
-elementoBotao.setAttribute( 'onclick', 'addTarefa()')
